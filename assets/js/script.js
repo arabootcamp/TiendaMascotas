1a6df1bdd930c9238d2995d2fa45ab636e822b8e
aracelly <aracellybootcamp@gmail.com>
2021-06-16 04:18:58 -0400

    cambios navbar, carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 
+$(function() {
+  // Handler for .ready() called.
 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+
+});
 
 
